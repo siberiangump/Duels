8ecd318fa2a6da91da9d781d272354eaa15f43d8
Антон Соколов <dantedarkt@gmail.com>
2017-05-13 12:00:49 +0300

    imported base packages and cube tracking controller

---
+++ Assets/BaseVRInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VR = UnityEngine.VR;
+
+public class BaseVRInput : MonoBehaviour
+{
+
+	void Update ()
+    {
+        transform.localPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
+        transform.localRotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch);
+	}
+}
