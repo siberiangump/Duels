3578a8e207e3cba67d69bed43a1aea245b86c45c
Sergey Sereda <sergej.sereda@murka.com>
2017-05-13 12:40:20 +0300

    Added base room scene, added network manager, added base player class

---
+++ Assets/Scripts/player/BasePlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class BasePlayer : NetworkBehaviour, IShooter, IShootable
+{
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Shoot();
+        }
+    }
+
+    public void GetShot()
+    {
+        Debug.LogWarning("GetShot");
+    }
+
+    public void Shoot()
+    {
+        BasePlayer[] players = GameObject.FindObjectsOfType<BasePlayer>();
+
+        foreach (var player in players)
+        {
+            if (player != this)
+            {
+                player.GetShot();
+            }
+        }
+    }
+
+    [Command]
+    private void CmdShoot()
+    {
+        Shoot();
+    }
+}
