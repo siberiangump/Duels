a1aba0350bcfacaffbdb103fd71a958b3c09f389
Антон Соколов <dantedarkt@gmail.com>
2017-05-13 14:51:49 +0300

    Pestol added

---
+++ Assets/PestolGrab.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PestolGrab : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject Ungrabbed;
+    [SerializeField]
+    private GameObject Grabbed;
+    [SerializeField]
+    private float DistanceToGrab;
+    [SerializeField]
+    private GameObject HandPosition;
+
+    private void Update()
+    {
+        if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0)
+        {
+            Grab();
+            Debug.LogError("!");
+        }
+        if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0)
+        {
+            Grab();
+            Debug.LogError("&!");
+        }
+    }
+    
+    private void Grab()
+    {
+        Ungrabbed.SetActive(false);
+        Grabbed.SetActive(true);
+    }
+}
